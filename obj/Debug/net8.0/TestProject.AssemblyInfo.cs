3013736f698d2ccc8146d967ace5be52ecd30d82
ramer klipel <ramerh@outlook.com>
2025-02-05 22:34:59 -0300

    b

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1116d8a7f8b4ca3494ed2a6f0787ac69b1c77e3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c5ed4d45ec2758d7bd5318c80908c254804a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
